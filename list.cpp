79de21b27df64ba85b94ef38f3267934e444f94c
Konstantin <ksharutin@gmail.com>
2016-06-24 23:40:18 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,148 +1,99 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
- 
-struct TPoint
-{
-   int x, y;
- 
-   TPoint(): x(0), y(0) { ; }
-   TPoint(int theX, int theY): x(theX), y(theY) { ; }
-};
- 
-struct TNode
-{
-   TPoint point;
-   TNode* next;
-   TNode* prev;
-};
- 
-//----------------------------------------------//
-// Добавление элемента в список. Добавление происходит в
-// начало списка, а не в конец, хотя исправить - минутное дело
-// Параметры: list -  сам список
-//            point - данные для добавления
-TNode* Push(TNode*& list, const TPoint& point)
-{
-   // Создаём новый узел
-   TNode* node = new TNode;
-   node->point = point;
- 
-   // В случае пустого списка просто закольцовываем
-   // на себя
-   if (list == NULL)
-   {
-      node->next = node->prev = node;
-   }
-   // В случае не пустого списка, вставляем созданный
-   // элемент в начало списка
-   else
-   {
-      // Сначала расставляем указатели в созданном элементе
-      node->next = list;
-      node->prev = list->prev;
-      // А теперь скрепляем с имеющимся
-      list->prev = node;
-      node->prev->next = node;
-   }
- 
-   list = node;
- 
-   return list;
+/создание циклического однонаправленного списка 
+void Make_Circle_Single_List(int n,
+       Circle_Single_List** Head,Circle_Single_List* Loop){
+    if (n > 0) {
+        (*Head) = new Circle_Single_List();
+        //выделяем память под новый элемент
+        if (Loop == NULL) Loop = (*Head);
+        cout << "Введите значение ";
+        cin >> (*Head)->Data;
+        //вводим значение информационного поля
+        (*Head)->Next=NULL;//обнуление адресного поля
+        Make_Circle_Single_List(n-1,&((*Head)->Next),Loop);
+    }
+    else {
+        (*Head) = Loop;
+    }
 }
-//----------------------------------------------//
-// Удаление элемента из списка
-// Параметры:  list -  сам список
-// Возвращает: значение удалённого узла
-TPoint Pop(TNode*& list)
-{
-   // Если список пуст, то закрываем лавочку
-   if (list == NULL)
-   {
-      return TPoint();
-   }
- 
-   // Сохраняем значение текущего узла, ибо он
-   // сейчас будет удалён
-   TPoint val(list->point);
-   TNode* node = list;
- 
-   // В случае если в списке был всего один элемент,
-   // то просто зануляем наш список
-   if ((list->next == list) || (list->prev == list))
-   {
-      list = NULL;
-   }
-   // Иначе перебиваем указатели буз учёта
-   // удаляемого элемента.
-   else
-   {
-      list->prev->next = list->next;
-      list->next->prev = list->prev;
-      list = list->next;
-   }
-   // Удаляем отцепленный узел
-   delete node;
- 
-   return val;
+
+//печать циклического однонаправленного списка
+void Print_Circle_Single_List(Circle_Single_List* Head) {
+    Circle_Single_List* ptr=Head; 
+    //вспомогательный указатель
+    do {
+       cout << ptr->Data << "\t";
+       ptr=ptr->Next;
+    } while (ptr!=Head);
+    cout << "\n"; 
 }
-//----------------------------------------------//
-// Перегрузка оператора вывода для структуры TPoint
-// P.S.: Себя капитаном очевидностью чувствую :(
-std::ostream& operator << (std::ostream& os, const TPoint& point)
-{
-   os << "[" << point.x << "," << point.y << "]";
- 
-   return os;
+
+/*вставка элемента после заданного номера в циклический однонаправленный список*/
+Circle_Single_List* Insert_Item_Circle_Single_List(Circle_Single_List* Head, 
+      int Number, int DataItem){
+  Circle_Single_List *Current = Head;
+  //встали на первый элемент
+  Circle_Single_List *NewItem = new(Circle_Single_List);
+  //создали новый элемент   
+  NewItem->Data = DataItem; 
+  if (Head == NULL) {//список пуст
+    NewItem->Next = NewItem;
+    Head = NewItem;
+  }
+  else {//список не пуст
+    for (int i = 1; i < Number; i++)
+      Current = Current->Next;
+    NewItem->Next = Current->Next;
+    Current->Next = NewItem;
+  }
+  return Head;
 }
-//----------------------------------------------//
-// Перегрузка для списка (struct TNode*)
-std::ostream& operator << (std::ostream& os, const TNode* node)
-{
-   if (node)
-   {
-      const TNode* cursor = node;
- 
-      for (; cursor->next != node; cursor = cursor->next)
-      {
-         os << cursor->point << " ";
-      }
-      os << cursor->point;
-   }
-   else
-   {
-      os << "nil";
-   }
- 
-   return os;
+
+/*удаление элемента с заданным номером из циклического однонаправленного списка*/
+Circle_Single_List* Delete_Item_Circle_Single_List
+      (Circle_Single_List* Head, int Number){
+  if (Head != NULL){
+    Circle_Single_List *Current = Head;
+    if (Head->Next != Head){
+      for (int i = 1; i < Number; i++)
+        Current = Current->Next;
+        Circle_Single_List *ptr = Head;
+      while (ptr->Next != Current)
+        ptr = ptr->Next;
+      //непосредственное удаление элемента
+      ptr->Next = Current->Next;
+      if (Head = Current) Head = Current->Next;
+      delete(Current);
+    }
+    else{
+      Head = NULL;
+      delete(Current);
+    }
+  }
+  return Head;
 }
-//----------------------------------------------//
- 
-int main()
-{
-   srand(time(NULL));
- 
-   TNode* list = NULL;
- 
-   // Случайным образом задаём 5 точек и добавляем их в список
-   // После чего сразу же выводим (и саму точку и список)
-   for (int i = 0; i < 5; ++i)
-   {
-      TPoint point(rand()%100 - 50, rand()%100 - 50);
-      Push(list, point);
-      std::cout << "addition: " << point << std::endl;
-      std::cout << "list: " << list << std::endl;
-   }
- 
-   // Пока в списке есть хоть один элемент
-   // удаляем верхний элемент в списке и выводим результат
-   while (list)
-   {
-      std::cout << "deleted: " << Pop(list) << std::endl;
-      std::cout << "list: "  << list << std::endl;
-   }
- 
-   system("pause");
- 
-   return 0;
+
+//поиск элемента в циклическом однонаправленном списке
+bool Find_Item_Circle_Single_List(Circle_Single_List* Head, 
+         int DataItem){
+  Circle_Single_List *ptr = Head; 
+  //вспомогательный указатель
+  do {
+    if (DataItem == ptr->Data) return true; 
+    else ptr = ptr->Next;
+    }
+  while (ptr != Head);
+  return false;
+}
+
+//проверка пустоты циклического однонаправленного списка
+bool Empty_Circle_Single_List(Circle_Single_List* Head){
+  return (Head != NULL ? false : true);
+}
+
+//удаление циклического однонаправленного списка
+void Delete_Circle_Single_List(Circle_Single_List* Head){
+  if (Head != NULL){
+    Head = Delete_Item_Circle_Single_List(Head, 1);
+    Delete_Circle_Single_List(Head);
+  } 
 }
